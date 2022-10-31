6ea19c1502df1153edc12c01a2c9e286f7986d09
Trevin Shu <trevinshu2008@gmail.com>
2022-10-30 20:57:38 -0600

    started web portfolio v3

---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="flex flex-col justify-center items-center gap-3 p-10">
+      <h1 className="text-zinc-800 text-4xl text-center font-bold uppercase tracking-widest lg:text-5xl">Trevin Shu</h1>
+      <p className="text-zinc-600 text-xl text-center uppercase tracking-widest underline decoration-wavy decoration-indigo-800 underline-offset-4">Web Developer</p>
+    </header>
+  );
+}
+
+export default Header;
