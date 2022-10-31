17f2010b045f07cfdbfc68132d1400c36e610d64
Trevin Shu <trevinshu2008@gmail.com>
2022-10-31 02:09:21 -0600

    finished skills section

---
+++ components/Timeline.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Timeline() {
+  return (
+    <div className="flex flex-col gap-5">
+      <h2 className="text-2xl uppercase tracking-widest underline decoration-wavy decoration-indigo-800 underline-offset-4">Timeline</h2>
+    </div>
+  );
+}
+
+export default Timeline;
