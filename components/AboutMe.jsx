30d3bad5186afa4318d3e6f3f6233b5f3cc8b019
Trevin Shu <trevinshu2008@gmail.com>
2022-10-30 22:44:31 -0600

    added about me container

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import Image from 'next/image'
+import Image from 'next/image';
+import ProfilePic from '../public/img/profilepic-medium.jpg';
 function AboutMe() {
-  return <div>
-
-    
-  </div>;
+  return (
+    <div className="flex flex-col gap-10 p-5 items-center md:flex-row">
+      <>
+        <Image src={ProfilePic} alt="profile pic" layout="fill" priority="true" className="rounded-xl" width={350} />
+      </>
+      <div>
+        <p className="text-xl text-zinc-800 text-left tracking-widest  break-words leading-loose">
+          Web Developer based in Edmonton, Alberta, Canada. I am passionate about building and delivering Web Applications that are aesthetically pleasing, feature rich, responsive and compliant with
+          the latest accessibility standards.
+          <br />
+          In my free time I like to learn new technologies, binge watch the latest movie or tv show, listen to music or play video games.
+        </p>
+        <button className="mt-5 border-solid border-zinc-800 border-[3px] px-5 py-3 rounded-sm hover:bg-zinc-800 hover:text-slate-50">Download/View My Resume</button>
+      </div>
+    </div>
+  );
 }
 
 export default AboutMe;
