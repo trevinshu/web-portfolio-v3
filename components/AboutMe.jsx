f8b4069c2eb0b3c361d6424d501edf08bbcee027
Trevin Shu <trevinshu2008@gmail.com>
2022-11-03 19:32:25 -0600

    adjusted image size

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -6,19 +6,19 @@ function AboutMe() {
   return (
     <div className="flex flex-col gap-10 items-center md:flex-row">
       <>
-        <Image src={ProfilePic} alt="profile pic" layout="fill" priority="true" className="w-1/2 h-auto rounded-md 2xl:w-1/5" sizes="100vw" width="404" height="500" />
+        <Image src={ProfilePic} alt="profile pic" priority="true" width="3840" height="2412" className="w-1/2 m-auto sm:w-1/3 xl:w-1/4 2xl:w-[15%]" />
       </>
       <div className="flex flex-col justify-center items-start gap-5">
         <Heading>About Me</Heading>
-        <p className="text-lg text-zinc-800 text-left tracking-widest  break-words leading-loose xl:text-xl">
+        <p className=" text-zinc-800 text-left tracking-widest  break-words leading-loose">
           I am a Web Developer based in Edmonton, Alberta, Canada. I am passionate about building and delivering Web Applications that are aesthetically pleasing, feature rich, responsive and
           compliant with the latest accessibility standards.
         </p>
-        <p className="text-lg text-zinc-800 text-left tracking-widest  break-words leading-loose xl:text-xl">
+        <p className=" text-zinc-800 text-left tracking-widest  break-words leading-loose">
           In my free time I like to learn new technologies, binge watch the latest movie or tv show, listen to music or play video games.
         </p>
         <a
-          className="text-xl border-solid border-zinc-800 border-[3px] px-5 py-3 rounded-sm hover:scale-110 hover:transition-all hover:ease-in-out"
+          className="text-lg text-center border-solid border-zinc-800 border-[3px] px-5 py-3 rounded-sm hover:scale-110 hover:transition-all hover:ease-in-out"
           href="resources/Trevin Shu - Resume 2022.pdf"
           target="_blank"
           rel="noreferrer"
