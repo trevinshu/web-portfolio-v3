194dc4f1154792f610b08fd12b4b1ebb5c0dab68
Trevin Shu <trevinshu2008@gmail.com>
2022-12-20 23:41:06 -0700

    altered header sub title

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className="flex flex-col justify-center items-center gap-3">
       <h1 className="text-zinc-800 text-4xl text-center font-bold uppercase tracking-widest lg:text-5xl">Trevin Shu</h1>
-      <p className="text-zinc-600 text-xl text-center uppercase tracking-widest underline decoration-wavy decoration-indigo-800 underline-offset-8">Web Developer</p>
+      <p className="text-zinc-600 text-xl text-center uppercase tracking-widest underline decoration-wavy decoration-indigo-800 underline-offset-8">Web Applications Developer</p>
     </header>
   );
 }
